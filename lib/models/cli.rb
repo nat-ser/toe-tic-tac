884bf32616b74aead3cc7554a1e1876f36ceed5a
Nataliya Seryakova <nataliya.seryakova@gmail.com>
2017-10-07 17:28:43 -0400

    some validations

--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -25,12 +25,12 @@ class CLI
     end
   end
 
-  def self.translate(key)
+  def self.valid_coordinate_map
     {
       q: 0, w: 1, e: 2,
       a: 3, s: 4, d: 5,
       z: 6, x: 7, c: 8,
-    }[key]
+    }
   end
 
   def self.display_board(board)
@@ -63,13 +63,21 @@ class CLI
 
   def self.prompt_human(human_name)
     puts "what's your move #{human_name}?"
-    key = gets.chomp
-    translate(key.to_sym)
+    key = gets.chomp.to_sym
+    until valid_move?(key, valid_coordinate_map)
+      puts "please enter one of these: #{valid_coordinate_map.keys}"
+      key = gets.chomp.to_sym
+    end
+    valid_coordinate_map[key]
   end
 
   def self.ask_x_or_o
     puts "do you prefer to play with an x or an o?"
-    gets.chomp
+    sign = gets.chomp
+    until valid_sign_choice?(sign)
+      puts "X OR O"
+      sign = gets.chomp
+    end
   end
 
   def self.ask_human_name
@@ -77,7 +85,11 @@ class CLI
     gets.chomp
   end
 
+  def self.valid_move?(key, valid_coordinate_map)
+    valid_coordinate_map.keys.include?(key)
+  end
 
-  def validate_input
+  def self.valid_sign_choice?(sign)
+    ["x", "o"].include?(sign)
   end
 end
