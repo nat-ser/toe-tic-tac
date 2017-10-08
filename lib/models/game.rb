27469962efc388a3c63953ca2abe0ef12839cf97
Nataliya Seryakova <nataliya.seryakova@gmail.com>
2017-10-08 16:18:40 -0400

    works w victory -- needs refactor

--- lib/models/game.rb
+++ lib/models/game.rb
@@ -1,59 +1,82 @@
 class Game
   attr_accessor :board, :human, :computer, :current_player
+  attr_reader :board_dimension, :indeces, :horizontal_victories, :vertical_victories, :diagonal_victories
 
   def initialize(human:, computer:)
     # TODO dont hardcode
     @board = Board.new([ 0, 1, 2, 3, 4, 5, 6, 7, 8])
-    @board_dimension = Math.sqrt(@board.squares.length)
     @human = human
     @computer = computer
     set_computer_sign
+
     @human.board = @board
     @computer.board = @board
+
+    @board_dimension = Math.sqrt(@board.squares.length).to_i
+    @indeces = (0..@board.squares.length).to_a
+    @increment = @board_dimension - 1
+    @first_row = (0..@increment).to_a
+
+    @horizontal_victories = set_horizontal_victories
+    @vertical_victories = set_vertical_victories
+    @diagonal_victories = set_diagonal_victories
     randomize_first_move
   end
 
   def victory?
-    # horizontal_victory?
-    vertical_victory?
-    # horizontal_victory? || diagonal_victory? || vertical_victory?
+    type_victory?(@horizontal_victories) || type_victory?(@vertical_victories) || type_victory?(@diagonal_victories)
   end
 
-  def vertical_victory?
-    sign = current_player.sign
-    squares = board.squares
-    increment = @board_dimension - 1
-
-    counts = (0..increment)
-    markers = (0..increment).each do |marker|
-      counts[marker] = 1 if squares[marker] == sign
+  def type_victory?(victories)
+    victories.each do |vic|
+      return true if (vic - (current_player.taken_squares)).empty?
     end
-    puts "MARKERS #{markers}"
-    squares.each_with_index do |square, idx|
-      next if idx < increment
     false
-
   end
 
-  def horizontal_victory?
-    sign = current_player.sign
-    increment = @board_dimension - 1
-    squares = board.squares
+  def set_horizontal_victories
     pointer = 0
-    while pointer < squares.length
-      segment = squares[pointer..(pointer + increment)]
-      condition_met = segment.all? { |square| square == sign }
-      if condition_met
-        puts "#{current_player.player_name} won!"
-        return true
-      else
-        pointer += increment
+    victories = []
+
+    @board_dimension.times do
+      victories << ((pointer..(pointer + @increment)).to_a)
+      pointer += @board_dimension
+    end
+    victories
+  end
+
+  def set_vertical_victories
+    victories = []
+    @board_dimension.times { victories << []}
+
+    @first_row.each do |victory_idx|
+      pointer = victory_idx
+      @board_dimension.times do
+        victories[victory_idx] << @indeces[pointer]
+        pointer += @board_dimension
       end
     end
+    victories
   end
 
-  def diagonal_victory?
-    false
+
+  def set_diagonal_victories
+    victories = [[], []]
+
+    pointer = 0
+    first_increment = @board_dimension + 1
+    @board_dimension.times do
+      victories[0] << @indeces[pointer]
+      pointer += first_increment
+    end
+
+    second_increment = @board_dimension - 1
+    pointer = @increment
+    @board_dimension.times do
+      victories[1] << @indeces[pointer]
+      pointer += second_increment
+    end
+    victories
   end
 
   def set_computer_sign
@@ -71,12 +94,14 @@ class Game
   def turn
     if current_player.is_a?(Human)
       human.move(CLI.prompt_human(human.player_name))
-      return if victory? || draw?
-      @current_player = computer
     else
       computer.move
-      return if victory? || draw?
-      @current_player = human
     end
+
+    if victory? || draw?
+      puts "yay #{current_player.player_name} won!"
+      return
+    end
+    @current_player = (current_player.is_a?(Human) ? computer : human)
   end
 end
