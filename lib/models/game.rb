39343a4d1efcfb6beb153724f5d8612cbaeafa28
Nataliya Seryakova <nataliya.seryakova@gmail.com>
2017-10-08 12:29:57 -0400

    some winning conditions

--- lib/models/game.rb
+++ lib/models/game.rb
@@ -1,9 +1,10 @@
 class Game
   attr_accessor :board, :human, :computer, :current_player
 
-  def initialize(human, computer)
+  def initialize(human:, computer:)
     # TODO dont hardcode
     @board = Board.new([ 0, 1, 2, 3, 4, 5, 6, 7, 8])
+    @board_dimension = Math.sqrt(@board.squares.length)
     @human = human
     @computer = computer
     set_computer_sign
@@ -12,8 +13,48 @@ class Game
     randomize_first_move
   end
 
-  # def victory?
-  # end
+  def victory?
+    # horizontal_victory?
+    vertical_victory?
+    # horizontal_victory? || diagonal_victory? || vertical_victory?
+  end
+
+  def vertical_victory?
+    sign = current_player.sign
+    squares = board.squares
+    increment = @board_dimension - 1
+
+    counts = (0..increment)
+    markers = (0..increment).each do |marker|
+      counts[marker] = 1 if squares[marker] == sign
+    end
+    puts "MARKERS #{markers}"
+    squares.each_with_index do |square, idx|
+      next if idx < increment
+    false
+
+  end
+
+  def horizontal_victory?
+    sign = current_player.sign
+    increment = @board_dimension - 1
+    squares = board.squares
+    pointer = 0
+    while pointer < squares.length
+      segment = squares[pointer..(pointer + increment)]
+      condition_met = segment.all? { |square| square == sign }
+      if condition_met
+        puts "#{current_player.player_name} won!"
+        return true
+      else
+        pointer += increment
+      end
+    end
+  end
+
+  def diagonal_victory?
+    false
+  end
 
   def set_computer_sign
     @computer.sign = ( @human.sign == "x" ? "o" : "x" )
@@ -24,15 +65,17 @@ class Game
   end
 
   def randomize_first_move
-    current_player = ( rand(0..1) == 1 ? human : computer )
+    @current_player = ( rand(0..1) == 1 ? human : computer )
   end
 
-  def switch_turn
+  def turn
     if current_player.is_a?(Human)
       human.move(CLI.prompt_human(human.player_name))
+      return if victory? || draw?
       @current_player = computer
     else
       computer.move
+      return if victory? || draw?
       @current_player = human
     end
   end
