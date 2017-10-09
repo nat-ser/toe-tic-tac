64f385c739cddde5f5cd05f684c3f54171dcfe89
Nataliya Seryakova <nataliya.seryakova@gmail.com>
2017-10-09 12:07:32 -0400

    extra validation and messages

--- lib/models/game.rb
+++ lib/models/game.rb
@@ -93,15 +93,23 @@ class Game
 
   def turn
     if current_player.is_a?(Human)
-      human.move(CLI.prompt_human(human.player_name))
+      human.move(CLI.prompt_human(player_name: human.player_name, board: board))
     else
+      puts "computer's move:"
       computer.move
     end
+    return if end_game_if_win_or_draw
 
-    if victory? || draw?
-      puts "yay #{current_player.player_name} won!"
-      return
-    end
     @current_player = (current_player.is_a?(Human) ? computer : human)
+  end
+
+  def end_game_if_win_or_draw
+    if victory?
+      puts current_player.is_a?(Human) ? "yay #{current_player.player_name} won!" : "computer won"
+      true
+    elsif draw?
+      puts "TIE"
+      true
+    end
   end
 end
